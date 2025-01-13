2d889ef1be88c2858a9e893f1c1b2e2ed62ab72d
kennethoyahebholo <oyahebholokennethizosea@gmail.com>
2025-01-13 14:34:36 +0100

    fixed image issue

--- src/screens/LandingPage/_partials/Blogs/index.tsx
+++ src/screens/LandingPage/_partials/Blogs/index.tsx
@@ -49,6 +49,15 @@ const Blogs = () => {
         <div className="bg-accent z-0 w-full h-full absolute top-0 left-0 bg-opacity-10 rounded-[1.3rem] animate-pulse"></div>
         <Image
           src={image}
+          height="3500"
+          width="1500"
+          loading="lazy"
+          alt="plan-img"
+          className="w-full h-[350px] md:w-[95%] md:h-[95%] lg:w-[90%] lg:h-[426px]"
+          style={{ color: "transparent" }}
+        />
+        {/* <Image
+          src={image}
           alt={title}
           loading="lazy"
           decoding="async"
@@ -63,7 +72,7 @@ const Blogs = () => {
             objectFit: "cover",
             color: "transparent",
           }}
-        ></Image>
+        ></Image> */}
       </div>
       <div className="pl-5 relative before:absolute before:w-[1px] before:h-[90%] before:bg-white before:left-0 before:top-[50%] before:-translate-y-[50%] group-hover:translate-x-4 custom-animate">
         <p className="text-lg font-semibold mb-1 text-white">Blog</p>
